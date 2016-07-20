4bf6db743b2a5656c5f37e35907c1c47897efd1d
AnatZ <Anat.Zeller@gmail.com>
2016-07-20 10:32:36 +0300

    Replaced XML Config with simple easy to use text settings file

--- AppRunner/Program.cs
+++ AppRunner/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Windows.Forms;
-
+using System.IO;
 namespace AppRunner
 {
     static class Program
@@ -13,28 +11,62 @@ namespace AppRunner
         [STAThread]
         static void Main(string[] args)
         {
+            string exeFile = "";
+            string workingDir = "";
+            string CommandLineArgs = "";
             try
             {
-                var p = new System.Diagnostics.Process();
-                string commandLine = Properties.Settings.Default.CommandLineArgs;
-                if (string.IsNullOrWhiteSpace(commandLine))
-                    commandLine = SplitCommandLine(System.Environment.CommandLine)[1];
+                using (var sr = new StreamReader(Path.GetFileNameWithoutExtension(AppDomain.CurrentDomain.FriendlyName) + ".settings"))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        var eq = line.IndexOf('=');
+                        if (eq > 0)
+                        {
+                            var name = line.Remove(eq).Trim().ToUpper() ;
+                            var value = line.Substring(eq + 1).Trim();
+                            if (name.StartsWith(";"))
+                                continue;
+                            switch (name)
+                            {
+                                case "EXEFILE":
+                                    exeFile = value;
+                                    break;
+                                case "WORKINGDIR":
+                                    workingDir = value;
+                                    break;
+                                case "COMMANDLINEARGS":
+                                    CommandLineArgs = value;
+                                    break;
+                                default:
+                                    
+                                    throw new Exception("Unknown setting " + name + "=" + value);
+                            }
+                        }
+                    }
 
-                p.StartInfo.Arguments = commandLine;
-                p.StartInfo.FileName = Properties.Settings.Default.ExeFile;
-                p.StartInfo.WorkingDirectory = Properties.Settings.Default.WorkingDirectory;
-                p.Start();
+                    var p = new System.Diagnostics.Process();
+                    string commandLine = CommandLineArgs;
+                    if (string.IsNullOrWhiteSpace(commandLine))
+                        commandLine = SplitCommandLine(System.Environment.CommandLine)[1];
+
+                    p.StartInfo.Arguments = commandLine;
+                    p.StartInfo.FileName = exeFile;
+                    p.StartInfo.WorkingDirectory = workingDir;
+                    p.Start();
+                }
             }
             catch (Exception ex)
             {
-                using (var sw = new System.IO.StringWriter())
+                using (var sw = new StringWriter())
                 {
                     sw.WriteLine("Failed: " + ex.Message);
-                    sw.WriteLine("ExeFile=" + Properties.Settings.Default.ExeFile);
-                    sw.WriteLine("WorkingDirectory=" + Properties.Settings.Default.WorkingDirectory);
-                    sw.WriteLine("CommandLineArgs=" + Properties.Settings.Default.CommandLineArgs);
+                    sw.WriteLine("ExeFile=" + exeFile);
+                    sw.WriteLine("WorkingDirectory=" + workingDir);
+                    sw.WriteLine("CommandLineArgs=" + CommandLineArgs);
                     sw.WriteLine("Called with=" + Environment.CommandLine);
-                    MessageBox.Show( sw.ToString(), "AppRunner Failed To Start", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    MessageBox.Show(sw.ToString(), "AppRunner Failed To Start", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
             }
         }
